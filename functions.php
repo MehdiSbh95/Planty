449d8986e04b5f95b8658c2f8b1270c6cb3828ab
MehdiSbh95 <133141064+MehdiSbh95@users.noreply.github.com>
2023-12-05 16:50:21 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,40 +10,6 @@ function theme_enqueue_styles(){
 }
 ?>
 
-<?php
-
-add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
-
-function add_admin_link( $items, $args ) {
-
-    if (is_user_logged_in() && $args->theme_location == 'menu-1') {
-
-        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
-
-    }
-
-    return $items;
-
-}
-
-?>
 
 
 
-
-
-<?php
-function planty_admin_link( $bard, $args ) {
-
-    if (is_user_logged_in() && $args->theme_location == 'header') {
-
-        $bard='<li id= "menu-item-42" class= "nav-item"><a href="http://localhost/Planty/nous-rencontrer/" class= "nav-link">Nous rencontrer</a></li>'.'<li class= "nav-item-link nav-item"><a href="'. get_admin_url() .'"'.' class= "nav-link">Admin</a></li>'.'<li id="menu-item-41" class= "nav-item"><a href="href=http://localhost/planty/commander/" class= "nav-link">Commander</a></li>';
-
-    }
-    return $bard;
-      
-}
-
-add_filter( 'wp_nav_menu_items','planty_admin_link', 10, 2 );
-
-?>
