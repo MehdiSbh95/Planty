205961f46e8c07e9c893dea45baed3f544b7df54
MehdiSbh95 <133141064+MehdiSbh95@users.noreply.github.com>
2023-12-05 16:51:00 +0100

    Update template-commander.php

--- template-commander.php
+++ template-commander.php
@@ -4,52 +4,7 @@
  */
 get_header();
 ?>
-<body class="body-commander">
 
-<form id="research">
-
-<div class="quantity-container">
-   <div>
-  <input type="text" id="quantity" value="1">
-  </div>
-  <div class="bouton-quantité">
-  <button id="plus">+</button>
-  <button id="minus">-</button>
-  </div>
-</div>
-
-<div class="quantity-container1">
-   <div>
-  <input type="text" id="quantity" value="1">
-  </div>
-  <div class="bouton-quantité">
-  <button id="plus">+</button>
-  <button id="minus">-</button>
-  </div>
-</div>
-
-<div class="quantity-container2">
-   <div>
-  <input type="text" id="quantity" value="1">
-  </div>
-  <div class="bouton-quantité">
-  <button id="plus">+</button>
-  <button id="minus">-</button>
-  </div>
-</div>
-
-<div class="quantity-container3">
-   <div>
-  <input type="text" id="quantity" value="1">
-  </div>
-  <div class="bouton-quantité">
-  <button id="plus">+</button>
-  <button id="minus">-</button>
-  </div>
-</div>
-
-</form>
-</body>
 <?php
 get_template_part( 'entry' );
 comments_template();
